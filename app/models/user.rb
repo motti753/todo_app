6705bee5a141007b93e3e51e79c5a1a748aca1a5
望月真紀 <yonetu.s.121@gmail.com>
2024-03-19 11:28:36 +0900

    taskテーブル作成。Userテーブルとの参照設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
   
   has_one_attached :avatar
   has_many :boards, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 
   def has_create?(board)
     boards.exists?(id: board.id)
