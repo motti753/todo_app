8944067d4eccfbd09069ea86392dba76caffc68b
望月真紀 <yonetu.s.121@gmail.com>
2024-03-22 20:28:15 +0900

    herokuのプロセス数が異常に増えていたため、最小値を増やしてみた。

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+web: bundle exec puma -t 5:5 -p ${PORT:-3100} -e ${RACK_ENV:-development}
 web: bundle exec puma -C config/puma.rb
 release: bundle exec rake db:migrate
--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 1 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
@@ -15,7 +15,7 @@ threads min_threads_count, max_threads_count
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
+  workers worker_count if worker_count > 5
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
